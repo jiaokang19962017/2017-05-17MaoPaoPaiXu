6522f3e8e0599ea4c602ea70cd14e6638e932cda
jiaokang <Administrator@HP201-1>
2017-05-17 10:24:55 +0800

    枚举

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,43 +10,53 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int[] number = new int[10] { 20, 5, 17, 28, 19, 15, 29, 87, 65, 32 };
-            int temp;
-            for (int i = 0; i < number.Length; i++)
-            {
-                for (int j = 0; j < number.Length-1; j++)
-                {
-                    if (number[j] < number[i])
-                    {
-                        temp = number[j];
-                        number[j] = number[j+1];
-                        number[j+1] = temp;
-                    }
-                    
-                }
-            }
-            for (int i = 0; i < number.Length; i++)
-            {
-                Console.WriteLine(number[i]);
-            }
+            /* int[] number = new int[10] { 20, 5, 17, 28, 19, 15, 29, 87, 65, 32 };
+             int temp;
+             for (int i = 0; i < number.Length; i++)
+             {
+                 for (int j = 0; j < number.Length-1; j++)
+                 {
+                     if (number[j] < number[i])
+                     {
+                         temp = number[j];
+                         number[j] = number[j+1];
+                         number[j+1] = temp;
+                     }
 
-          /*  for (int i = 1; i < number.Length; i++)
-            {
-                for (int j = 0; j < number.Length - i; j++)
-                {
-                    if (number[j] < number[j] + 1)
-                    {
-                        int temp;
-                        temp = number[j];
-                        number[j] = number[j + 1];
-                        number[j+1] = temp;
-                    }
-                }
-            }
-            for (int i = 0; i < number.Length; i++)
-            {
-                Console.Write(number[i]+" ");
-            }*/
+                 }
+             }
+             for (int i = 0; i < number.Length; i++)
+             {
+                 Console.WriteLine(number[i]);
+             }
+
+           /*  for (int i = 1; i < number.Length; i++)
+             {
+                 for (int j = 0; j < number.Length - i; j++)
+                 {
+                     if (number[j] < number[j] + 1)
+                     {
+                         int temp;
+                         temp = number[j];
+                         number[j] = number[j + 1];
+                         number[j+1] = temp;
+                     }
+                 }
+             }
+             for (int i = 0; i < number.Length; i++)
+             {
+                 Console.Write(number[i]+" ");
+             }*/
+
+
+
+            #region 枚举
+      
+
+
+
+            #endregion
         }
     }
 }
+
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    enum color { yellow, blue, green, blac, red };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                  //枚举是一种固定的集合
+            // enum Weekday { Sun,Mon,Tue,Wed,Thu,Fri,Sat};
+
+        Console.WriteLine("yellow={0}",color.yellow);
+        }
+    }
+}
